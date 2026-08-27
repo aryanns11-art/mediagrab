d710ea3137b093dc6493662a01f5e31f9a4b2f81
aryanns11-art <aryanns1104@gmail.com>
2026-08-27 16:32:31 +0530

    feat: display YouTube video information and quality options

--- app/main_window.py
+++ app/main_window.py
@@ -8,7 +8,8 @@ from PySide6.QtWidgets import (
     QLabel,
     QLineEdit,
     QPushButton,
-    QMessageBox
+    QMessageBox,
+    QComboBox
 )
 from url_utils import is_valid, detect_platform
 from youtube_service import get_video_info
@@ -20,6 +21,7 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("MediaGrab")
         self.setMinimumSize(900, 600)
 
+        self.video_info = None
         self.setup_ui()
 
     def setup_ui(self):
@@ -40,15 +42,35 @@ class MainWindow(QMainWindow):
         self.analyze_button = QPushButton("Analyze URL")
 
         self.platform = QLabel('Platform - ')
+        self.title_label = QLabel('Title:-')
+        self.channel_label= QLabel('Channel-')
+        self.duration_label = QLabel('Duration:-')
+        self.quality_label = QLabel('Quality:-')
+        self.quality_combo = QComboBox()
+        self.quality_combo.addItem('Best')
 
         self.analyze_button.clicked.connect(self.analyze_url)
 
         layout.addWidget(title)
         layout.addWidget(subtitle)
+
         layout.addSpacing(20)
         layout.addWidget(self.url_input)
         layout.addWidget(self.analyze_button)
+
+        layout.addSpacing(10)
         layout.addWidget(self.platform)
+
+        layout.addSpacing(20)
+        layout.addWidget(self.title_label)
+
+        layout.addWidget(self.channel_label)
+        layout.addWidget(self.duration_label)   
+
+        layout.addSpacing(10)
+        layout.addWidget(self.quality_label)
+        layout.addWidget(self.quality_combo)  
+
         layout.addStretch()
 
     def analyze_url(self):
@@ -69,22 +91,44 @@ class MainWindow(QMainWindow):
 
         if platform == "YouTube":
             try:
-                info=get_video_info(url)
+                self.video_info = get_video_info(url)
 
-                title = info.get('title','Unknown Title')
-                channel = info.get('channel','Unknown Channel')
-                duration = info.get('duration',0)
-
-                min = duration // 60
-                sec = duration % 60
-
-                QMessageBox.information(self,'Video Info',
-                                        f'Title: {title}\n'
-                                        f'Channel: {channel}\n'
-                                        f'Duration: {min}m {sec}s')
-
+                self.display_video_info()
+                
             except Exception as e:
                 QMessageBox.critical(self,'Error',
                                     'Failed to retrieve video information.'
                                     f'Error: {e}')  
+
+
+
+    def display_video_info(self):
+
+            title = self.video_info["title"]
+            channel = self.video_info["channel"]
+            duration = self.video_info["duration"]
+
+            minutes = duration // 60
+            seconds = duration % 60
+
+            self.title_label.setText(f"Title: {title}")
+
+            self.channel_label.setText(f"Channel: {channel}")
+
+            self.duration_label.setText(f"Duration: {minutes}:{seconds:02d}")
+
+            self.quality_combo.clear()
+
+            self.quality_combo.addItem("Best")
+
+            qualities = set()
+
+            for video_format in self.video_info["formats"]:
+
+                height = video_format["height"]
+                qualities.add(height)
+
+            for height in sorted(qualities,reverse=True):
+
+                self.quality_combo.addItem(f"{height}p")
 
--- app/youtube_service.py
+++ app/youtube_service.py
@@ -10,4 +10,31 @@ def get_video_info(url):
 
     with yt_dlp.YoutubeDL(options) as ydl:
         info = ydl.extract_info(url, download=False)
-        return info
+
+
+    format = []
+
+
+    for video_format in info.get('formats',[]):
+
+        height = video_format.get('height')
+        ext = video_format.get('ext')
+        video_codec = video_format.get('vcodec')
+        audio_codec = video_format.get('acodec')
+
+        if(height and ext and video_codec and audio_codec):
+            format.append({
+                'format_id':video_format.get('format_id'),
+                'height':height,
+                'ext':ext,
+                'video_codec':video_codec,
+                'audio_codec':audio_codec
+            })
+
+    return {
+        'title': info.get('title','Unknown'),
+        'channel': info.get('channel','Unknown Channel'),
+        'duration':info.get('duration',0),
+        'thumbnail':info.get('thumbnail','Unknown Thumbnail'),
+        'formats': format
+    }
